8b1bb2f75d92e991f12dacaf943e7afb0f3d18f8
sq2ips <pawel.sq2ips@gmail.com>
2024-08-03 16:36:16 +0200

    fix

--- config_baltyk.py
+++ config_baltyk.py
@@ -150,7 +150,7 @@ activitymap = ActivityMap(
 from baltyk_sq2ips import BaltykSq2ips
 
 baltyksq2ips = BaltykSq2ips(
-    language=pl_google,
+    language=lang,
     service_url="https://baltyk.imgw.pl//getdata/forecast.php?type=sea&lang=pl",
     # service_url="http://91.220.17.153/index-maps/forecastGetData.php?type=sea&lang=pl", # niepewne źródło
     # service_url="http://baltyk.pogodynka.pl/index-maps/forecastGetData.php?type=sea&lang=pl", # stary, nie działający URL
