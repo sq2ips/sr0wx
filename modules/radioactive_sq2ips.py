0d5e59df7f32bff5f9faeba4fa55b388fbece55e
sq2ips <pawel.sq2ips@gmail.com>
2025-12-14 17:44:25 +0100

    More fixes in radioactive_sq2ips

--- modules/radioactive_sq2ips.py
+++ modules/radioactive_sq2ips.py
@@ -3,9 +3,6 @@ from datetime import datetime, timedelta
 
 from sr0wx_module import SR0WXModule
 
-# https://monitoring.paa.gov.pl/_api/maps/MapLayer/15d20873-f8a7-8899-5d69-960cc9ebbbb6/DetailsTable/f5af6ec4-d759-3163-344e-cbf147d28e28/Data/d2e87d20-28e2-47ea-860d-98a4e98d8726?dateFrom=2025-11-12T00:00:00.000Z&dateTo=2025-12-12T00:00:00.000Z
-# https://monitoring.paa.gov.pl/_api/maps/MapLayer/15d20873-f8a7-8899-5d69-960cc9ebbbb6/DetailsTable/f5af6ec4-d759-3163-344e-cbf147d28e28/Data/d2e87d20-28e2-47ea-860d-98a4e98d8726?dateFrom=2025-12-12T00:00:00.000Z&dateTo=2025-12-12T00:00:00.000Z
-
 class RadioactiveSq2ips(SR0WXModule):
     """Klasa pobierająca dane o promieniowaniu z PAA"""
 
@@ -25,7 +22,7 @@ class RadioactiveSq2ips(SR0WXModule):
 
     def processData(self, data):
         datediff = datetime.now() - datetime.strptime(data["date_end_str"], "%Y-%m-%d %H:%M")
-
+        print(datediff)
         if datediff < timedelta(days=1):
             pass
         elif datediff >= timedelta(days=1) and datediff < timedelta(days=2):
@@ -34,22 +31,23 @@ class RadioactiveSq2ips(SR0WXModule):
             raise ValueError(f"Dane są nieaktualne o więcej niż jeden dzień, różnica czasów wynosi {datediff}")
         
         val = round(float(data["moc_dawki"]), 2)
-        self.__logger.info(f"Wartość z czujnika, data: {str(datetime.strptime(data['date_end_str'], '%Y-%m-%d %H:%M'))}: {val}")
+        self.__logger.info(f"Wartość aktualna z czujnika, data: {data["date_end_str"]}: {val} uSv/h")
         return val
     
     def processDataSr(self, data):
         val_sr = 0
         for reading in data:
             val_sr+=round(float(reading["moc_dawki"]), 2)
-        val_sr/=len(data)
+        val_sr= round(val_sr/len(data), 2)
+        self.__logger.info(f"Średnia wartość z ostatnich {len(data)}, pomiarów: {val_sr} uSv/h")
         return(val_sr)
 
 
     def get_data(self):
         data = self.request()
-        value = self.processData(data[0])
+        value = self.processData(data[-1])
         value_sr = self.processDataSr(data)
-        self.__logger.info("Wartość przetwożona: " + str(value))
+
         va = round(value * 100)
         curentValue = " ".join(
             [
@@ -61,7 +59,6 @@ class RadioactiveSq2ips(SR0WXModule):
         )
         if value_sr is not None:
             va_sr = round(value_sr*100)
-            self.__logger.info("Średnia wartość przetwożona: " + str(va_sr / 100))
             averageValue = " ".join(
                 [
                     "s_rednia_wartos_c__dobowa",
